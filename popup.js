a58c9254f0649ef08154a3776d6e6067c597f145
jonnydaw <jonathandaw29@gmail.com>
2024-08-23 14:35:28 +0100

    block linkedin

--- popup.js
+++ popup.js
@@ -1,9 +1,7 @@
 
-// to-do remove from blocked list
-
 const blocked = {};
 const blockForm =  document.querySelector("#blockForm");
-const validWebsites = ["reed.co.uk"]
+const validWebsites = ["reed.co.uk", "linkedin.com"]
 
 // chrome.storage.local.clear(function() {
 //     var error = chrome.runtime.lastError;
@@ -31,66 +29,68 @@ blockForm.addEventListener("submit", (event) => {
             alert("Invalid Input");
         return;
     }
-    blocked[blockForm.website.value] = blockForm.recruiter.value; 
+    blocked[blockForm.website.value.trim()] = blockForm.recruiter.value.trim(); 
     updateUserSettingsAddition(blocked);
     blockForm.reset();
+    blockForm.website.focus();
     
 })
 
-// blockForm.addEventListener("keydown", (event) => {
-    
-//     if(event.key === 'Enter'){
-//         event.preventDefault();
-//     if(blockForm.website.value === "" || blockForm.recruiter.value === ""){
-//         return;
-//     }
-//     blocked[blockForm.website.value] = blockForm.recruiter.value; 
-//     updateUserSettingsAddition(blocked);
-//     blockForm.reset();
-// }
-    
-// })
 
 const updateUserSettingsAddition = async (params) => {
-    // check if already in
-    let userSettings = await getUserSettings();
-    let arr = []
-    
-    const key = Object.keys(params)[0];
-    const value = Object.values(params)[0];
-    
-    if(key in userSettings){
-        arr = [...userSettings[key], value];
-    } else{
-        arr.push(value);
+    try {
+
+        let userSettings = await getUserSettings();
+        let arr = []
+        
+        const key = Object.keys(params)[0];
+        const value = Object.values(params)[0];
+        
+        if(key in userSettings){
+            arr = [...userSettings[key], value];
+        } else{
+            arr.push(value);
+        }
+        setUserSettings(key,arr)
+
+    } catch (error) {
+        throw new Error(`Addition to blocked list failed ${error}`);
+        
     }
-    setUserSettings(key,arr)
+    
 }
 
 const updateUserSettingsRemoval = async (key, value) => {
     // check if not in
 
-    let userSettings = await getUserSettings();
-    let arr = []
-
-    if(key in userSettings && userSettings[key].includes(value)){
-        arr = userSettings[key];
-        arr = arr.filter(item => item !== value);
-        if(arr.length === 0){
-            chrome.storage.sync.remove(key, () => {alert("removed")})
+    try {
+        let userSettings = await getUserSettings();
+        let arr = []
+    
+        if(key in userSettings && userSettings[key].includes(value)){
+            arr = userSettings[key];
+            arr = arr.filter(item => item !== value);
+            if(arr.length === 0){
+                chrome.storage.sync.remove(key, () => {alert("removed")})
+            }
         }
+        setUserSettings(key,arr);
+    } catch (error) {
+        throw new Error(`Removal from blocked list failed ${error}`);
     }
-    setUserSettings(key,arr);
-    //alert(JSON.stringify(await getUserSettings()));
     
 }
 
 const getUserSettings = async () => {
-    let res;
-     await chrome.storage.sync.get().then((result) => {
-        res = result;
-    });
-    return res;
+    try {
+        let res;
+        await chrome.storage.sync.get().then((result) => {
+            res = result;
+        });
+        return res;
+    } catch (error) {
+        throw new Error(`Retrieval from blocked list failed ${error}`);
+    }
 }
 
 const setUserSettings = async (key, arr) => {
--- scripts/content.js
+++ scripts/content.js
@@ -21,23 +21,38 @@ const interval =  setInterval(async () =>{
     userSettings = await getUserSettings();
 
     if (userSettings["reed.co.uk"] && currentUrl.includes("reed.co.uk")){
-      blockReedJobs(userSettings["reed.co.uk"])
+        blockReedJobs(userSettings["reed.co.uk"])
+    } 
+    else if (userSettings["linkedin.com"] && currentUrl.includes("linkedin.com")){
+      blockLinkedinJobs(userSettings["linkedin.com"])
     }
   }
 }, 1500);
 
 
-const blockReedJobs = (recruiters)  =>{
+const blockReedJobs = (recruiters)  =>{ 
   const jobCards  = document.querySelectorAll("[data-element='recruiter']")
       jobCards.forEach((job) =>{
-        let parent = job.closest("article");
-        if(recruiters.includes(job.textContent)){
+        const parent = job.closest("article");
+        if(recruiters.includes(job.textContent.trim())){
           parent.style.display = 'none'; 
         } else if(parent?.style.display == "none"){
           parent.style.display = "block"
         }
-      })
-      
+      }) 
   }
+
+const blockLinkedinJobs = (recruiters) => {
+    const jobCards = document.querySelectorAll(".job-card-container__primary-description");
+    jobCards.forEach((job) => {
+      const parent = job.closest("li");
+      console.log(job.textContent.trim())
+        if(recruiters.includes(job.textContent.trim())){
+          parent.style.display = 'none'; 
+        } else if(parent?.style.display == "none"){
+          parent.style.display = "block"
+        }
+    })
+}
 
 
